c9862d1d352e7e8bdf40411daab4a562a9df8a21
Bikoev Konstantin <bikoev_kv@mail.ru>
2020-12-06 20:54:42 +0500

    fix one mistake

--- server4000.js
+++ server4000.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const url = require('url');
 const server = new http.Server();
-server.listen(4200, '127.0.0.1');
+server.listen(4000, '127.0.0.1');
 
 server.on('request', function (req, res) {
     const urlParsed = url.parse(req.url, true);
